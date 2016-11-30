db2ea3473f08e0853def50ad644c5b3a5af96418
Ram Kota <ram.kota@abbott.com>
2016-11-30 15:13:07 -0600

    Updated config file to remove optional values

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-exports.handler = (event, context, callback) => callback(null, "Hello World!!!");
+exports.handler = (event, context, callback) => 
+{
+    callback(null, "Hello World!!!")
+};
--- lambda-config.js
+++ lambda-config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  accessKeyId: '',  // optional
-  secretAccessKey: '',  // optional
-  sessionToken: '',  // optional
-  profile: '', // optional for loading AWS credientail from custom profile
   region: 'us-east-1',
   handler: 'index.handler',
-  role: '',
-  functionName: '<function name>',
+  functionName: 'test',
   timeout: 10,
   memorySize: 128,
   publish: true, // default: false,
@@ -14,10 +9,5 @@ module.exports = {
   vpc: { // optional
     SecurityGroupIds: [],
     SubnetIds: []
-  },
-  eventSource: {
-    EventSourceArn: '<event source such as kinesis ARN>',
-    BatchSize: 200,
-    StartingPosition: "TRIM_HORIZON"
   }
 }
